5302a84edf9eaea22b8e9ec0cf21d0cf09295a43
tta2yta <tta2yta@gmail.com>
2021-08-17 11:31:38 +0300

    Update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,28 @@
 import React from "react"
+import { NavLink  } from "react-router-dom"
 
 const NavBar=()=>{
+    const links = [
+        {
+          id: 1,
+          path: "/",
+          text: "Home",
+        },
+        {
+          id: 2,
+          path: "/about",
+          text: "About",
+        },
+      ]
     return (
-        <div>Nav Bar</div>
+        <div className="navBar">
+            <ul>
+                {links.map(item=>{
+                return    <NavLink  to={item.path}
+                activeClassName="active-link">{item.text}</NavLink >
+                })}
+            </ul>
+        </div>
     );
 }
 
